54714f962a2998d24d8db491f5d529ccecf708e1
Jeffrey France <jbfrance1626@gmail.com>
2026-06-05 22:36:15 +0800

    feat: add Docker support with Dockerfile and docker-compose.yml, and update environment variables

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from '../auth/auth.module';
 import { ChatMessage } from './chat-message.entity';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 
 @Module({
   controllers: [ChatController],
-  imports: [TypeOrmModule.forFeature([ChatMessage])],
+  imports: [TypeOrmModule.forFeature([ChatMessage]), AuthModule],
   providers: [ChatService],
 })
 export class ChatModule {}
